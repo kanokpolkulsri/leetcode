84eca592e273205683eeb5b54875585200f07d74
tonplamm <tonplam_kanokpol@hotmail.com>
2021-02-20 21:57:59 +0700

    feat: biweekly_46 formarray

---
+++ biweekly_46/formarray.cpp
@@ -0,0 +1,12 @@
+/*
+loop in nums:
+    cn = char from nums
+    loop in groups:
+        bring first char in the group
+        check if it's equal to cn:
+            plus current nums and check with each char in group
+        if the group is found:
+            delete from group
+if !group: true
+else: false
+*/
